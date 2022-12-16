f34e1d18bd6f1ab763121ec50288a4983cd53ddc
JoseReyes1504 <eduardo.reyessuazo@gmail.com>
2022-12-16 15:32:23 -0600

    Mejora Responsive Movil

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,17 @@
-import { onAuthStateChanged , auth } from "./db.js";
+import { onAuthStateChanged, auth } from "./db.js";
+
+
+export var Usuario = "";
 
 onAuthStateChanged(auth, (user) => {
-  if (!user) {
-    location.href='../../index.html';    
-  }
+    if (!user) {
+        location.href = '../../index.html';
+    }
+
+    if (user.email === "jose.reyessuazo@gmail.com") {
+        Usuario = "Tato";        
+    } else {
+        Usuario = "Alejandra";
+    }
+    console.log(Usuario);
 });
